c48c519c908d67ca79de582dd26ee313a70a8a31
Wade <wadefh@gmail.com>
2021-11-17 20:53:08 -0800

    JS finished, need to do bug testing

--- script.js
+++ script.js
@@ -53,12 +53,19 @@ function operate(){     //calls other functions to do math with current numbers
                     
                 }
                 calculation.answer = Math.round((calculation.answer + Number.EPSILON) * 100) / 100; //round to 2 decimal places
-        
-                calculation.firstNumber = calculation.answer; //set first number to answer
+                
+                
+
+                calculation.firstNumber = calculation.answer.toString(); //set first number to answer
                 calculation.onFirstNumber = true; 
                 
+                if(checkLength(calculation.answer.toString())){
+                    displayNumber(calculation.answer.toExponential(2));
+                }
+                else{              
+                    displayNumber(calculation.answer);
+                }
                 displayFormula(calculation.lastOperation);
-                displayNumber(calculation.answer);
             }
     return; 
     }
@@ -82,7 +89,17 @@ function clear(){   //clears everything to default
     document.getElementById('formula').innerHTML = "&nbsp";  
 }
 
-function addNumber(number){
+function checkLength(number){           //check length of current number to not overflow screen needs a string        
+   
+    number = number.replace('.' , '');
+    number = number.replace('-', '');
+
+    console.log(number, typeof number)
+
+    return (number.length >= maxLength);
+}
+
+function addNumber(number){ //adds to current number on screen
     if(calculation.onFirstNumber){  //add to first number if they haven't added an operator 
         calculation.firstNumber = calculation.firstNumber + number;
         displayNumber(calculation.firstNumber);
@@ -93,7 +110,20 @@ function addNumber(number){
     }
 }
 
-function addDecimal(){
+function removeNumber(){ //backspace
+
+    if(calculation.onFirstNumber){  //add to first number if they haven't added an operator 
+              
+        calculation.firstNumber = calculation.firstNumber.slice(0, -1);
+        displayNumber(calculation.firstNumber);
+    }
+    else{ //add to second number should only function if operator has been selected already 
+        calculation.secondNumber = calculation.secondNumber.slice(0, -1);
+        displayNumber(calculation.secondNumber);
+    }
+}
+
+function addDecimal(){ //adds decimal to current number
 
     let x = Number(calculation.firstNumber)
     let y = Number(calculation.secondNumber)
@@ -119,10 +149,43 @@ function addDecimal(){
     }     
 }
 
+function numberSign(){ //sets negative or positive of number
+
+    let x = Number(calculation.firstNumber)
+    let y = Number(calculation.secondNumber) 
+       
+    console.log(x,y)
+
+
+    if( x > 0 || y > 0){
+        if(calculation.onFirstNumber){  
+            calculation.firstNumber = "-" + calculation.firstNumber;
+            displayNumber(calculation.firstNumber);
+        }
+        else{ 
+            calculation.secondNumber = '-' + calculation.secondNumber;
+            displayNumber(calculation.secondNumber);
+        }       
+    }
+    else{
+        if(calculation.onFirstNumber){  
+            calculation.firstNumber = calculation.firstNumber.slice(1);
+            displayNumber(calculation.firstNumber);
+        }
+        else{ 
+            calculation.secondNumber = calculation.secondNumber.slice(1);
+            displayNumber(calculation.secondNumber);
+        }         
+    }
+
+}
+
 function addOperator(operator){     //handles adding operator and decision if first number is new or answer from previous equation
     
-    
-    if(calculation.onFirstNumber === false){
+    if(calculation.firstNumber === ''){
+        return;
+    }    
+    else if(calculation.onFirstNumber === false){
         operate();
     }   
     
@@ -138,7 +201,14 @@ function buttonPress(pressButton){
         case '1': case '2': case '3': 
         case '4': case '5': case '6': 
         case '7': case '8': case '9':
-            addNumber(pressButton);
+            let numberCheck = (calculation.onFirstNumber)? calculation.firstNumber: 
+                                                           calculation.secondNumber;  
+            if(checkLength(numberCheck)){
+                break;
+            }
+            else{
+                addNumber(pressButton);
+            }
             break;
         case 'decimal':
             addDecimal();            
@@ -148,16 +218,23 @@ function buttonPress(pressButton){
         case 'multiply':
         case 'divide':
             addOperator(pressButton);
+            break;
+        case 'numbersign':
+            numberSign();
             break;        
         case 'clear':
             clear();
+            break;
+        case 'delete':
+            removeNumber();
             break;
         case 'equals': 
             operate();
             break;
         default:   //do nothing 
             break;
-    };    
+    };
+    console.table(calculation);    
 }
 
 function keyInput(keyPressed){
@@ -187,13 +264,20 @@ function keyInput(keyPressed){
             case 'c':
                 document.getElementById('clear').click();               
                 break;
-            case '=': 
+            case '=': case 'Enter': 
                 document.getElementById('equals').click(); 
                 break;
+            case 'Backspace':
+                document.getElementById('delete').click();  
+                break;  
             default:   //do nothing return out of function
+            console.log(keyPressed);
             return;       
     }
+console.log(keyPressed);
 }
+
+let maxLength = 7; //set max length for screen
 
 let calculation = { firstNumber: '', 
                     secondNumber: '', 
@@ -203,7 +287,7 @@ let calculation = { firstNumber: '',
                     lastOperation: '',
                     answer: ''};
 
-const buttons = document.querySelector("#allButtons");
+const buttons = document.querySelector("#buttonGrid");
 const displayScreen = document.getElementById("#display");
 const formulaScreen = document.getElementById("#formula");
 
@@ -216,3 +300,5 @@ buttons.addEventListener('click', button => {
 });
 
 window.addEventListener('keydown', e => keyInput(e.key));
+
+/* need to implement the backspace and +/- key as awell as string length check */
